7be717a961f5d041dc018bb44128aedf1655de25
anujsinghx7 <167003190+anujsinghx7@users.noreply.github.com>
2025-09-10 09:37:20 +0530

    Disabled the inspect feature.

--- script.js
+++ script.js
@@ -334,5 +334,23 @@ fetch('https://leetcode-stats-api.herokuapp.com/anujsinghx7')
       Ranking: ${data.ranking}
     `;
   });
+    document.addEventListener("contextmenu", (e) => e.preventDefault());
+
+  document.addEventListener("keydown", (e) => {
+    if (e.key === "F12") {
+      e.preventDefault();
+    }
 
   
+    if (e.ctrlKey && e.shiftKey && (e.key.toLowerCase() === "i" || e.key.toLowerCase() === "j" || e.key.toLowerCase() === "c")) {
+      e.preventDefault();
+    }
+
+    
+    if (e.ctrlKey && e.key.toLowerCase() === "u") {
+      e.preventDefault();
+    }
+  });
+
+  
+
